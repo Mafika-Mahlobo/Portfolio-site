a71b73bf9558542cc2f79d3d5cc351cc6da6b6f6
Mafika <rudolphmafika@gmail.com>
2025-11-03 10:49:36 +0200

    Slide show - Initial

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,3 +7,21 @@ const section = document.getElementById('intro-text');
       });
     }, { threshold: 0.1 });
     observer.observe(section);
+
+
+    // Simple slideshow functionality
+    document.addEventListener('DOMContentLoaded', () => {
+      // Image slider functionality
+      const slideContainers = document.querySelectorAll('.slide-container');
+      slideContainers.forEach(container => {
+      let currentIndex = 0;
+      const slides = container.querySelectorAll('.slide-image');
+      slides[currentIndex].style.display = 'block';
+
+      setInterval(() => {
+        slides[currentIndex].style.display = 'none';
+        currentIndex = (currentIndex + 1) % slides.length;
+        slides[currentIndex].style.display = 'block';
+      }, 3000); // Change image every 3 seconds
+    });
+  });
