1498d2bf41fbd34fe7ad47d53ec82b750e8d46b5
Mafika <rudolphmafika@gmail.com>
2025-11-04 12:38:01 +0200

    Final portfolio MVP

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -25,3 +25,36 @@ const section = document.getElementById('intro-text');
       }, 3000); // Change image every 3 seconds
     });
   });
+
+  const resumeBtn = document.querySelector(".resume-button");
+  const mobileMenuIcon = document.querySelector(".fa-bars");
+
+  resumeBtn.addEventListener("click", () => {
+      const link = document.createElement("a");
+      link.href = "/static/documents/Mafika_Mahlobo.pdf";
+      link.download = "Mafika_Mahlobo.pdf";
+      link.click();
+  });
+
+  document.querySelector(".app1-download").addEventListener("click", () => {
+      const link = document.createElement("a");
+      link.href = "/static/documents/cargoflow.apk";
+      link.download = "cargoflow.apk";
+      link.click();
+  });
+
+  mobileMenuIcon.addEventListener("click", () => {
+      document.querySelector(".mobile-nav").style.display = "block";
+  });
+
+  document.querySelector(".cancel-mobile-container").addEventListener("click", () => {
+    document.querySelector(".mobile-nav").style.display = "none";
+  });
+
+  document.querySelector(".comming").addEventListener("click", () => {
+    document.querySelector(".overly").style.display = "block";
+  });
+
+  document.querySelector(".cancel").addEventListener("click", () => {
+      document.querySelector(".overly").style.display = "none";
+  });
