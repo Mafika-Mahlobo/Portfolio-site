f5918618e9288197bf968a2657d75ac2880559b5
Mafika <rudolphmafika@gmail.com>
2026-02-18 08:35:42 +0200

    feat: setup profile controller

---
+++ controllers/profile.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+
+
+exports.addProfile = (req, res) => {
+    const { name, email, hero, bio } = req.body;
+    let profilePic;
+    let resume;
+
+
+    try {
+    
+
+    } catch (error) {
+        
+    }
+}
