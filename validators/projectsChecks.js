6eb28fcfcfc22cfd3c21b129459f218d863bc999
Mafika <rudolphmafika@gmail.com>
2026-03-18 09:58:29 +0200

    fix: Fix spinner issue

--- validators/projectsChecks.js
+++ validators/projectsChecks.js
@@ -2,9 +2,9 @@ const { check } = require('express-validator');
 
 const projectsCheck = [
     check('title', 'Title field is required').trim().notEmpty(),
-    check('title', 'You\'ve exceede the allowed character count').isLength({max: 20}),
+    check('title', 'You\'ve exceede the allowed character count').isLength({max: 500}),
     check('description', 'Description field is required').notEmpty(),
-    check('description', 'You\'ve exceede the allowed character count').isLength({max: 500}),
+    check('description', 'You\'ve exceede the allowed character count').isLength({max: 3000}),
 ];
 
 module.exports = projectsCheck;
