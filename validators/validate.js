19a07c1778b0c4269c82472efdf8f4fa986ced49
Mafika <rudolphmafika@gmail.com>
2026-03-18 10:27:38 +0200

    fix: Fix alerts issue

--- validators/validate.js
+++ validators/validate.js
@@ -9,7 +9,7 @@ const validate = (req, res, next) => {
     }
     
     // return errors
-    return res.status(400).json({errors: errors.array()});
+    return res.status(401).json({errors: errors.array()});
 
 };
 
